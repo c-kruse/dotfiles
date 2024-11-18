0086f20542ac47ceafdba748ca1bc4ffbeb23c59
Christian Kruse <christian@c-kruse.com>
2024-11-18 12:37:14 -0800

    wip
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- install.sh
+++ install.sh
@@ -12,7 +12,10 @@ if ! command -v nvim 2>&1 > /dev/null; then
 	echo "install nvim"
 	exit 1
 fi
-cp -r nvim $HOME/.config/
-cp -r bin $HOME/.local/bin
-cp -r .bashrc .bashrc.d $HOME
-cp -r .tmux.conf .gitconfig .ssh $HOME
+cp -r nvim "$HOME/.config/"
+cp -r bin "$HOME/.local/bin"
+if [ -f "$HOME/.bashrc" ]; then
+	mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
+fi
+cp -r .bashrc .bashrc.d "$HOME"
+cp -r .tmux.conf .gitconfig .ssh "$HOME"
